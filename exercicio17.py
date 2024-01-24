d70052ff5f470d4a4e87c078706c52e1c3a4c4be
Vinicius Luna <viniciusluna@yahoo.com>
2024-01-24 15:31:47 -0300

    second commit

--- exercicio17.py
+++ exercicio17.py
@@ -7,4 +7,12 @@ while True:
             break
         valores.append(valor)
         produto += 1
-    total
+    total = sum(valores)
+    dinheiro = float(input("Digite a quatidade de dinheiro entregue pelo cliente: "))
+    troco = dinheiro - total
+    for i in range(len(valores)):
+        print(f'Produto {i + 1}: {valores[i]}')
+    print(f'Total: {total}')
+    print(f'Dinheiro: {dinheiro}')
+    print(f'Troco: {troco}')
+
